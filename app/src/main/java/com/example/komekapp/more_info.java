4b42b36d75a8af285133614b6c790026bdeff29d
ansarzeinulla <mussabaeva47@bk.ru>
2023-09-24 19:43:56 +0600

    24.09 Adding

--- app/src/main/java/com/example/komekapp/more_info.java
+++ app/src/main/java/com/example/komekapp/more_info.java
@@ -1,14 +1,65 @@
 package com.example.komekapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class more_info extends AppCompatActivity {
-
+Integer a;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_more_info);
+
+        Button btn=findViewById(R.id.more_info_give_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatabaseReference reference= FirebaseDatabase.getInstance().getReference("listOfInclusives");
+                reference.addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                        String loginOfI=snapshot.getValue(String.class);
+                        DatabaseReference referencePOINT=FirebaseDatabase.getInstance().getReference("inclusives").child(loginOfI);
+                        referencePOINT.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshotP) {
+                                a= snapshotP.child("points").getValue(Integer.class);
+                                Log.e("___",loginOfI+"/"+a.toString());
+                                DatabaseReference reference1=FirebaseDatabase.getInstance().getReference("inclusives").child(loginOfI);
+                                reference1.child("points").setValue(a+10);
+                            }
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+                            }
+                        });
+                    }
+                    @Override
+                    public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                    }
+                    @Override
+                    public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+                    }
+                    @Override
+                    public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                    }
+                });
+            }
+        });
     }
 }
